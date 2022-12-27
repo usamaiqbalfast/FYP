af7a42ce5bc69c2e8e7e404c825c44a473971ac0
unknown <usamaiqbalfast>
2022-12-28 01:53:59 +0500

    basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Table from './components/Table';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <Navbar />
+      <br></br>
+      
+      <h3 >List of Books</h3>
+      <Table />
+      <Footer />
+
+      
     </div>
   );
 }
